995e25130b46b252b11d76f992a578493d3dd653
Vanessa Nascimento <101233011+Nayayaa@users.noreply.github.com>
2023-09-01 19:08:51 -0300

    Ac2.py

--- Ac2.py
+++ Ac2.py
@@ -30,8 +30,6 @@ print("A soma do triplo do primeiro com o terceiro é:", resultado2)
 print("O terceiro elevado ao cubo é:", resultado3)
 
 
-
-
 # Terceiro exercicio
 def calculo_oposto(num1, num2, num3):
     resultado1 = 2 * num1 * num2 / 2
@@ -45,6 +43,4 @@ num3 = float(input("Informe o terceiro número: "))
 
 resultado1, resultado2, resultado3 = calculo_oposto(num1, num2, num3)
 
-print("O produto do dobro do primeiro com metade do segundo é:", resultado1)
-print("A soma do triplo do primeiro com o terceiro é:", resultado2)
-print("O terceiro elevado ao cubo é:", resultado3)
+print("O valor de todos é:", resultado1, resultado2, resultado3)
